dbba6210e418bcde42e9ac57d0016503d69152d0
Harsha <harshasinha99@gmail.com>
2020-06-06 18:06:25 +0530

    Adding AWS changes

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5#v=)o07y)znriiq6t2b%i58u!+@lt@fo6eldmgc+9j*k=fisc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = []
 
@@ -121,5 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
